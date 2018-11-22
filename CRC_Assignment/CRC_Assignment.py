fe9a7be34c2de4abfe60bb03d1ab42c8451a9337
Mohamed <30885495+1OneOfAKind1@users.noreply.github.com>
2018-11-22 17:10:30 +0200

    CRC Division Function
    Add CRC division function

--- CRC_Assignment/CRC_Assignment.py
+++ CRC_Assignment/CRC_Assignment.py
@@ -1,3 +1,17 @@
+def crc_div(data,generator):
+    m = len(generator)                                  #generator length
+    frame = data + ("0"*(m-1))                          #frame with appended zeros
+    remainder = XOR(frame[:m], generator)               #first XOR
+    for i in range (len(data)-1):
+        remainder = remainder[1:m] + frame[m + i]       #delete most significant bit and add new bit from frame
+        if(remainder[0]=="1"):                          #check for XOR
+            remainder = XOR(remainder,generator)
+    remainder = remainder[1:m]                          #delete most significant bit
+    return remainder
+
+
+
+
 def verifier (transmitted_message,generator):  #it takes the message and the generator and checks if the reminder is zero or not to check errors in the messagge
     reminder_chk=int(crc_div(transmitted_message,generator),2)
     if(reminder_chk==0):
